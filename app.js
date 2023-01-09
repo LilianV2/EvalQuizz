542f2e6dbfd35aab28c42f952bf635477a248d1c
lilia <lilianom59@outlook.fr>
2023-01-09 10:41:50 +0100

    quizz

---
+++ app.js
@@ -0,0 +1,14 @@
+let quizz = [
+    {"number": 1, "question" : "Quel est le nom de famille de Naruto ?", "answer1" : "Uzukima", "answer2" : "Uzamuki", "answer3" : "Umazuki", "answer4" : "Uzumaki", "picture" : "img/naruto.jfif"},
+    {"number": 2, "question" : "Quel est le nom de famille du père de Naruto ?", "answer1" : "Uzukima", "answer2" : "Uzamuki", "answer3" : "Umazuki", "answer4" : "Uzumaki", "picture" : "img/naruto.jfif"},
+    {"number": 3, "question" : "Quel est le nom de famille de Naruto ?", "answer1" : "Uzukima", "answer2" : "Uzamuki", "answer3" : "Umazuki", "answer4" : "Uzumaki", "picture" : "img/naruto.jfif"},
+    {"number": 4, "question" : "Quel est le nom de famille de Naruto ?", "answer1" : "Uzukima", "answer2" : "Uzamuki", "answer3" : "Umazuki", "answer4" : "Uzumaki", "picture" : "img/naruto.jfif"},
+    {"number": 5, "question" : "Quel est le nom de famille de Naruto ?", "answer1" : "Uzukima", "answer2" : "Uzamuki", "answer3" : "Umazuki", "answer4" : "Uzumaki", "picture" : "img/naruto.jfif"},
+    {"number": 6, "question" : "Quel est le nom de famille de Naruto ?", "answer1" : "Uzukima", "answer2" : "Uzamuki", "answer3" : "Umazuki", "answer4" : "Uzumaki", "picture" : "img/naruto.jfif"},
+    {"number": 7, "question" : "Quel est le nom de famille de Naruto ?", "answer1" : "Uzukima", "answer2" : "Uzamuki", "answer3" : "Umazuki", "answer4" : "Uzumaki", "picture" : "img/naruto.jfif"},
+    {"number": 8, "question" : "Quel est le nom de famille de Naruto ?", "answer1" : "Uzukima", "answer2" : "Uzamuki", "answer3" : "Umazuki", "answer4" : "Uzumaki", "picture" : "img/naruto.jfif"},
+    {"number": 9, "question" : "Quel est le nom de famille de Naruto ?", "answer1" : "Uzukima", "answer2" : "Uzamuki", "answer3" : "Umazuki", "answer4" : "Uzumaki", "picture" : "img/naruto.jfif"},
+    {"number": 10, "question" : "Quel est le nom de famille de Naruto ?", "answer1" : "Uzukima", "answer2" : "Uzamuki", "answer3" : "Umazuki", "answer4" : "Uzumaki", "picture" : "img/naruto.jfif"},
+]
+
+$("#img").attr("src", quizz[0].picture)
